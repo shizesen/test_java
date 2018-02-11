dfab49abe583d7b8d0d68d152ed36ef4ba5d9346
shizesen <shizesen@gmail.com>
2018-02-11 22:18:28 +0800

    0211

--- src/lyons/page/GoodsPage.java
+++ src/lyons/page/GoodsPage.java
@@ -11,7 +11,9 @@ import lyons.tools.ScannerChoice;
  * Created by shizesen on 2018/2/5.
  */
 public final class GoodsPage extends ScannerChoice{
-
+    /**
+     * 操作商品界面
+     */
     public static void addGoodsPage()
     {
         System.out.println("\t执行添加商品操作：\n");
@@ -39,6 +41,9 @@ public final class GoodsPage extends ScannerChoice{
 
     }
 
+    /**
+     * 2更改商品界面
+     */
     public static void updateGoodsPage()
     {
         System.out.println("\t执行更改商品操作：\n");
@@ -58,15 +63,63 @@ public final class GoodsPage extends ScannerChoice{
             if (choice.matches(regex))
             {
                 int info = Integer.parseInt(choice);
-                switch (info)
-                {
+                switch (info) {
                     case 0:
                         MainPage.MaintenancePage();
                         break;
                     case 1:
-                        System.out.println();
+                        System.out.println("请输入商品新名称");
+                        String gname = ScannerInfoString();
+                        Goods goodsName = new Goods(gid, gname);
+                        boolean boolName = new GoodsDao().updateGoods(1, goodsName);
+                        if (boolName) {
+                            System.out.println("\t成功更新商品名称至数据库！");
+                        } else {
+                            System.out.println("\t更新商品名称失败");
+                        }
+                        changedInfoNext("updateGoodsPage");
+                        break;
+                    case 2:
+                        System.out.println("\t请输入商品新价格：");
+                        double gprice = ScannerInfo();
+                        Goods goodsPrice = new Goods(gid, gprice);
+                        boolean boolPrice = new GoodsDao().updateGoods(2, goodsPrice);
+                        if (boolPrice) {
+                            System.out.println("\t成功更新商品价格");
+                        } else {
+                            System.out.println("\t更新价格失败");
+                        }
+                        break;
+                    case 3:
+                        System.out.println("\t请输入商品新数量：");
+                        int gNum = ScannerNum();
+                        Goods goodsNum = new Goods(gid, gNum);
+                        boolean boolNum = new GoodsDao().updateGoods(3, goodsNum);
+                        if (boolNum) {
+                            System.out.println("\t成功更新商品数量");
+                        } else {
+                            System.out.println("\t更新商品数量失败");
+                        }
+                        break;
+                    default:
+                        System.out.println("\t请输入正确的选择");
+                        break;
                 }
-            }
+                System.out.println("\t输入有误，请重新选择，或者按0返回上一级菜单");
+                }
+            }while (true);
         }
+
+    /**
+     * 3删除商品界面
+     */
+    public static void deleteGoodsPage()
+    {
+        System.out.println("\t正在执行 删除商品 操作\n");
+        System.out.println("请输入想要删除的商品名称");
+
+        //调用查找商品函数，显示将要删除的商品
+        int gid = QueryPrint.query("deleteGoodsPage");
     }
 }
+
--- src/lyons/tools/QueryPrint.java
+++ src/lyons/tools/QueryPrint.java
@@ -1,8 +1,45 @@
 package lyons.tools;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import lyons.dao.GoodsDao;
+import lyons.db.*;
+import lyons.entity.Goods;
+import lyons.entity.SalesMan;
+
 /**
+ * 查询并打印函数工具
  * Created by shizesen on 2018/2/5.
  */
-public class QueryPrint {
+public final class QueryPrint {
+    Connection conn = null;
+    PreparedStatement pstmt = null;
+    ResultSet rs = null;
+
+    /**
+     * 模糊查询并陈列查询信息函数小工具
+     * @param oper 调用者
+     * @return 查询到的信息的gid，如果返回值等于-1，则代表查询异常
+     */
+    public static int query(String oper)
+    {
+        int gid= -1;
+        String shopping = ScannerChoice.ScannerInfoString();//键盘获取商品名称
+        ArrayList<Goods> goodsList = new QueryPrint().queryGoodsKey(-1,shopping);//根据键盘获取的商品名字調用 精确查询函数，確定用戶所要操作的数据
+        if (goodsList == null || goodsList.size() <= 0)
+        {
+            System.out.println("\t查无此商品");
+
+            //调用选择下一步函数
+            ScannerChoice.changedInfoNext(oper);
+        }else {//查到有此商品
+
+        }
+
+    }
 
 }
